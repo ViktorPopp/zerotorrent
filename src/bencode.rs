165cdf4e6f6415f3cb6f696c219b60b43c9a204e
Viktor Popp <viktorpopp.dev@gmail.com>
2025-10-30 12:39:14 +0100

    Added support for Bencode numbers

--- src/bencode.rs
+++ src/bencode.rs
@@ -1,6 +1,20 @@
 /// Decodes a Bencode-encoded string and returns a JSON value representing the parsed data along with any remaining unprocessed input.
 pub fn decode_value(encoded_value: &str) -> (serde_json::Value, &str) {
     match encoded_value.chars().next() {
+        Some('i') => {
+            if let Some((n, rest)) =
+                encoded_value
+                    .split_at(1)
+                    .1
+                    .split_once('e')
+                    .and_then(|(digits, rest)| {
+                        let n = digits.parse::<i64>().ok()?;
+                        Some((n, rest))
+                    })
+            {
+                return (n.into(), rest);
+            }
+        }
         Some('0'..='9') => {
             if let Some((len, rest)) = encoded_value.split_once(':') {
                 if let Ok(len) = len.parse::<usize>() {
