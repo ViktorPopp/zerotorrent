bc8441b16b54e0742c5c12fab9470cc0ae9240e5
Viktor Popp <viktorpopp.dev@gmail.com>
2025-10-30 12:34:56 +0100

    Added Bencode decoding for strings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use clap::{Parser, Subcommand, command};
+
+mod bencode;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    #[command(about = "Decodes a Bencoded string")]
+    Decode {
+        #[arg()]
+        value: String,
+    },
+}
+
 fn main() {
-    println!("ZeroTorrent - Open-Source BitTorrent Client");
+    let args = Args::parse();
+    match &args.command {
+        Commands::Decode { value } => {
+            let (value, rest) = bencode::decode_value(value);
+            println!("Decoded Value: {value}");
+            if !rest.is_empty() {
+                println!("Remainder:     \"{rest}\"");
+            }
+        }
+    }
 }
